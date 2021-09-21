6a400cbcf5ebd27cd27c096250420651535542d8
mkowalewski <aspoka1@gmail.com>
2021-09-21 10:42:26 +0200

    Add measuring data dto

---
+++ kayak-app-backend/src/main/java/pl/mkowalewski/kayakapp/measurs/domain/dto/MeasuringDataDto.java
@@ -0,0 +1,11 @@
+package pl.mkowalewski.kayakapp.measurs.domain.dto;
+
+import lombok.Data;
+import pl.mkowalewski.kayakapp.stations.domain.dto.StationDto;
+
+@Data
+public class MeasuringDataDto {
+  private Long id;
+  private StationDto station;
+  private Integer waterLevel;
+}
