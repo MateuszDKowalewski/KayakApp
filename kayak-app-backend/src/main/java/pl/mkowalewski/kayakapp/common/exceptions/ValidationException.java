83ddf23d504dd760d7d02509dc9db66a6615edf2
mkowalewski <aspoka1@gmail.com>
2021-09-22 19:36:39 +0200

    Fix validator error

--- kayak-app-backend/src/main/java/pl/mkowalewski/kayakapp/common/exceptions/ValidationException.java
+++ kayak-app-backend/src/main/java/pl/mkowalewski/kayakapp/common/exceptions/ValidationException.java
@@ -1,14 +1,24 @@
 package pl.mkowalewski.kayakapp.common.exceptions;
 
+import java.util.Map;
+import pl.mkowalewski.kayakapp.common.exceptions.ErrorResponse.ErrorResponseBuilder;
+
 public abstract class ValidationException extends ApplicationException {
 
-  public ValidationException(String message) {
+  private final Map<String, ValidationErrorType> validationError;
+
+  public ValidationException(String message, Map<String, ValidationErrorType> validationError) {
     super(message, ErrorType.VALIDATION_EXCEPTION);
+    this.validationError = validationError;
   }
 
   @Override
   public ErrorResponse getResponse() {
-    return null;
+    return new ErrorResponseBuilder()
+        .errorType(this.getErrorType())
+        .message(this.getMessage())
+        .validationError(this.validationError)
+        .build();
   }
 
 }
