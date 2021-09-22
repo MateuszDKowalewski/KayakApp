36c793f43e8c6ce5f27182cd77ecaa15f9a9625e
mkowalewski <aspoka1@gmail.com>
2021-09-22 15:00:14 +0200

    Annotation fix

--- kayak-app-backend/src/main/java/pl/mkowalewski/kayakapp/stations/domain/exceptions/ExceptionsHandler.java
+++ kayak-app-backend/src/main/java/pl/mkowalewski/kayakapp/stations/domain/exceptions/ExceptionsHandler.java
@@ -1,17 +1,15 @@
 package pl.mkowalewski.kayakapp.stations.domain.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.mkowalewski.kayakapp.common.exceptions.ErrorResponse;
 import pl.mkowalewski.kayakapp.common.exceptions.ErrorType;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionsHandler {
 
-  @ResponseBody
   @ExceptionHandler(StationNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ErrorResponse stationNotFoundHandler(StationNotFoundException e) {
